d08ac23f1eb448b09a4d1188fb5a652d93acd226
Wojuade Abdul Afeez <wojucrown7@gmail.com>
2025-02-28 08:33:08 +0100

    Add constants for base URL and African countries endpoint

---
+++ lib/src/core/constants/constants.dart
@@ -0,0 +1,4 @@
+//Not sure where the Constants file should go yet.
+class Constants {
+  static const String baseUrl = 'https://restcountries.com/v3.1/';
+}
---
+++ lib/src/core/constants/endpoints.dart
@@ -0,0 +1,4 @@
+class Endpoints {
+  static const String africanCountries =
+      "region/africa?status=true&fields=name,languages,capital,flags";
+}
