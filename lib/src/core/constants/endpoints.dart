edcd7153affe6e777bce246fadc6d82497d517dd
Wojuade Abdul Afeez <wojucrown7@gmail.com>
2025-02-28 10:31:22 +0100

    Add africanCountryDetails endpoint for fetching specific country details

--- lib/src/core/constants/endpoints.dart
+++ lib/src/core/constants/endpoints.dart
@@ -1,4 +1,4 @@
 class Endpoints {
-  static const String africanCountries =
-      "region/africa?status=true&fields=name,languages,capital,flags";
+  static const String africanCountries = "region/africa?status=true&fields=name,languages,capital,flags";
+  static const String africanCountryDetails = "name/";
 }
