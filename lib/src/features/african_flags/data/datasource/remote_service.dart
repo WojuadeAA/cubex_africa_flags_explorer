d31dd309e5c14c945dc39aeec558f0729189b296
Wojuade Abdul Afeez <wojucrown7@gmail.com>
2025-02-28 10:12:09 +0100

    Add getCountryDetails method to AfricanFlagsRemoteService for fetching country details

--- lib/src/features/african_flags/data/datasource/remote_service.dart
+++ lib/src/features/african_flags/data/datasource/remote_service.dart
@@ -1,10 +1,12 @@
 import 'package:cubex_africa_flags_explorer/src/core/constants/endpoints.dart';
 import 'package:cubex_africa_flags_explorer/src/core/network/network_service.dart';
 import 'package:cubex_africa_flags_explorer/src/core/network/response_processor.dart';
+import 'package:cubex_africa_flags_explorer/src/features/african_flags/data/models/country_details_model.dart';
 import 'package:cubex_africa_flags_explorer/src/features/african_flags/data/models/country_model.dart';
 
 abstract class AfricanFlagsRemoteService {
   Future<List<Country>> getCountries();
+  Future<CountryDetails> getCountryDetails(String countryName);
 }
 
 class AfricanFlagsRemoteServiceImpl implements AfricanFlagsRemoteService {
@@ -21,5 +23,16 @@ class AfricanFlagsRemoteServiceImpl implements AfricanFlagsRemoteService {
         return rawCountries.map((e) => Country.fromJson(e as Map<String, dynamic>)).toList();
       },
     );
+  }
+
+  @override
+  Future<CountryDetails> getCountryDetails(String countryName) async {
+    final response = await httpService.get('${Endpoints.africanCountryDetails}/$countryName');
+    return processResponse<CountryDetails>(
+        response: response,
+        serializer: (data) {
+          final rawCountryDetail = data as List;
+          return CountryDetails.fromJson(rawCountryDetail.first as Map<String, dynamic>);
+        });
   }
 }
